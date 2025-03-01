e7087f7dd7ead97fb2e1576dbf8a20b401eca5e8
csdarren <2001dhall@gmail.com>
2025-03-01 18:15:42 -0500

    Initial File Commit

---
+++ out/build/default/_deps/raylib-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/CPP/PewPewGame/out/build/default/_deps/raylib-src
+# Build directory: C:/dev/CPP/PewPewGame/out/build/default/_deps/raylib-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int, char **)
+{
+    char temp = 'c';
+    std::cout << "Hello, from PewPewGame!\n";
+    std::cin >> temp;
+}
